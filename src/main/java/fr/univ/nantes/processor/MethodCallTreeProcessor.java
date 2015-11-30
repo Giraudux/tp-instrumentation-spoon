809fced1b8e7b960175d6dfd934bee796df20a33
Giraudux <giraudux@gmail.com>
2015-11-30 19:08:12 +0100

    use properly spoon library

--- src/main/java/fr/univ/nantes/processor/MethodCallTreeProcessor.java
+++ src/main/java/fr/univ/nantes/processor/MethodCallTreeProcessor.java
@@ -1,7 +1,9 @@
 package fr.univ.nantes.processor;
 
 import spoon.processing.AbstractProcessor;
+import spoon.reflect.code.CtBlock;
 import spoon.reflect.code.CtStatement;
+import spoon.reflect.code.CtTry;
 import spoon.reflect.declaration.CtMethod;
 
 /**
@@ -12,11 +14,20 @@ public class MethodCallTreeProcessor extends AbstractProcessor<CtMethod> {
     @Override
     public void process(CtMethod method) {
         System.out.println(method.getParent().getSignature() + ": " + method.getSignature());
-        String enterCode = "fr.univ.nantes.logger.LogWriter.enterMethod(\"" + method.getParent().getSignature() + ": " + method.getSignature() + "\"); try {//";
-        String leaveCode = "} finally { fr.univ.nantes.logger.LogWriter.leaveMethod(); }//";
-        CtStatement enterStatement = getFactory().Code().createCodeSnippetStatement(enterCode);
-        CtStatement leaveStatement = getFactory().Code().createCodeSnippetStatement(leaveCode);
-        method.getBody().insertBegin(enterStatement);
-        method.getBody().insertEnd(leaveStatement);
+
+        String enterCode = "fr.univ.nantes.logger.LogWriter.enterMethod(\"" + method.getParent().getSignature() + ": " + method.getSignature() + "\")";
+        String leaveCode = "fr.univ.nantes.logger.LogWriter.leaveMethod()";
+        CtStatement enterCtStatement = getFactory().Code().createCodeSnippetStatement(enterCode);
+        CtStatement leaveCtStatement = getFactory().Code().createCodeSnippetStatement(leaveCode);
+        CtTry ctTry = getFactory().Core().createTry();
+        CtBlock finallyCtBlock = getFactory().Core().createBlock();
+        CtBlock bodyCtBlock = getFactory().Core().createBlock();
+
+        finallyCtBlock.addStatement(leaveCtStatement);
+        ctTry.setFinalizer(finallyCtBlock);
+        ctTry.setBody(method.getBody());
+        bodyCtBlock.addStatement(enterCtStatement);
+        bodyCtBlock.addStatement(ctTry);
+        method.setBody(bodyCtBlock);
     }
 }
