722d3af8287a89f1e44171e1d2bd6e952778f5a4
Giraudux <giraudux@gmail.com>
2015-11-30 22:52:50 +0100

    improve display

--- src/main/java/fr/univ/nantes/processor/MethodCallCounterProcessor.java
+++ src/main/java/fr/univ/nantes/processor/MethodCallCounterProcessor.java
@@ -2,18 +2,37 @@ package fr.univ.nantes.processor;
 
 import spoon.processing.AbstractProcessor;
 import spoon.reflect.code.CtStatement;
+import spoon.reflect.declaration.CtClass;
 import spoon.reflect.declaration.CtMethod;
+import spoon.reflect.declaration.CtParameter;
 
 /**
  * @author Alexis Giraudet
  * @author Thomas Minier
  */
-public class MethodCallCounterProcessor extends AbstractProcessor<CtMethod> {
+public class MethodCallCounterProcessor extends AbstractProcessor<CtMethod<?>> {
     @Override
-    public void process(CtMethod method) {
-        System.out.println(method.getParent().getSignature() + ": " + method.getSignature());
-        String code = "fr.univ.nantes.logger.LogWriter.call(\"" + method.getParent().getSignature() + ": " + method.getSignature() + "\")";
+    public void process(CtMethod<?> method) {
+        String code = "fr.univ.nantes.logger.LogWriter.call(\"" + getMethodKey(method) + "\")";
         CtStatement snippet = getFactory().Code().createCodeSnippetStatement(code);
+
         method.getBody().insertBegin(snippet);
+    }
+
+    private String getMethodKey(CtMethod<?> method) {
+        StringBuilder methodKey = new StringBuilder();
+        methodKey.append(method.getParent(CtClass.class).getSimpleName())
+                .append(".").append(method.getSimpleName())
+                .append("(");
+        for (CtParameter<?> parameter : method.getParameters()) {
+            methodKey.append(parameter.toString())
+                    .append(", ");
+        }
+        if (methodKey.charAt(methodKey.length() - 1) != '(') {
+            methodKey.delete(methodKey.length() - 2, methodKey.length());
+        }
+        methodKey.append(")");
+
+        return methodKey.toString();
     }
 }
--- src/main/java/fr/univ/nantes/processor/MethodCallTreeProcessor.java
+++ src/main/java/fr/univ/nantes/processor/MethodCallTreeProcessor.java
@@ -4,7 +4,9 @@ import spoon.processing.AbstractProcessor;
 import spoon.reflect.code.CtBlock;
 import spoon.reflect.code.CtStatement;
 import spoon.reflect.code.CtTry;
+import spoon.reflect.declaration.CtClass;
 import spoon.reflect.declaration.CtMethod;
+import spoon.reflect.declaration.CtParameter;
 
 /**
  * @author Alexis Giraudet
@@ -13,9 +15,7 @@ import spoon.reflect.declaration.CtMethod;
 public class MethodCallTreeProcessor extends AbstractProcessor<CtMethod> {
     @Override
     public void process(CtMethod method) {
-        System.out.println(method.getParent().getSignature() + ": " + method.getSignature());
-
-        String enterCode = "fr.univ.nantes.logger.LogWriter.enterMethod(\"" + method.getParent().getSignature() + ": " + method.getSignature() + "\")";
+        String enterCode = "fr.univ.nantes.logger.LogWriter.enterMethod(\"" + getMethodKey(method) + "\")";
         String leaveCode = "fr.univ.nantes.logger.LogWriter.leaveMethod()";
         CtStatement enterCtStatement = getFactory().Code().createCodeSnippetStatement(enterCode);
         CtStatement leaveCtStatement = getFactory().Code().createCodeSnippetStatement(leaveCode);
@@ -29,5 +29,22 @@ public class MethodCallTreeProcessor extends AbstractProcessor<CtMethod> {
         bodyCtBlock.addStatement(enterCtStatement);
         bodyCtBlock.addStatement(ctTry);
         method.setBody(bodyCtBlock);
+    }
+
+    private String getMethodKey(CtMethod<?> method) {
+        StringBuilder methodKey = new StringBuilder();
+        methodKey.append(method.getParent(CtClass.class).getSimpleName())
+                .append(".").append(method.getSimpleName())
+                .append("(");
+        for (CtParameter<?> parameter : method.getParameters()) {
+            methodKey.append(parameter.toString())
+                    .append(", ");
+        }
+        if (methodKey.charAt(methodKey.length() - 1) != '(') {
+            methodKey.delete(methodKey.length() - 2, methodKey.length());
+        }
+        methodKey.append(")");
+
+        return methodKey.toString();
     }
 }
